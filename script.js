8322a1db5ed7e2c550c1332291fbd03456b963c6
Guilherme Augusto <133021078+Guilherme-Augusto06@users.noreply.github.com>
2023-11-30 16:09:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,96 +1,176 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const tabela = document.querySelector(".tabela-js");
-  
-    axios.get(`https://api.augustin06.repl.co/list`)
-      .then(function(resposta) {
-        getData(resposta.data);
-      })
-      .catch(function(error) {
-        console.error(error);
+// script.js
+
+// Função para carregar as despesas na tabela
+async function carregarDespesas() {
+  try {
+      const response = await axios.get('http://127.0.0.1:5000/list');
+      const despesas = response.data;
+
+      const tabela = document.querySelector('.tabela-js');
+      tabela.innerHTML = '';
+
+      despesas.forEach(despesa => {
+          const tr = document.createElement('tr');
+          tr.innerHTML = `
+          <td>${despesa.ID}</td>
+          <td>${despesa.DESPESA}</td>
+          <td>R$ ${despesa.VALOR.toFixed(2)}</td>
+          <td>
+              <button class="btn bg-white delete-btn" type="button" data-bs-toggle="modal" data-bs-target="#modalDel" onclick="excluirDespesa(${despesa.ID})">
+                  <span class="material-symbols-outlined text-danger">delete</span>
+              </button>
+              <button class="btn bg-white edit-btn" id="edit-tarefa-btn" type="button" data-bs-toggle="modal" data-bs-target="#modalEdit" onclick="editarDespesa(${despesa.ID}, '${despesa.DESPESA}', ${despesa.VALOR})">
+                  <span class="material-symbols-outlined text-success">edit</span>
+              </button>
+          </td>
+          
+          `;
+          tabela.appendChild(tr);
       });
-  
-    function getData(dados) {
-      tabela.innerHTML = dados.map(item => `
-          <tr>
-          <th scope="row">${item.ID}</th>
-          <td>${item.TAREFA}</td>
-          <td><button class="btn bg-white delete-btn" type="button" data-bs-toggle="modal" data-bs-target="#modalDel"><span class="material-symbols-outlined text-danger">
-          delete
-          </span></button> <button class="btn bg-white edit-btn" id="edit-tarefa-btn"  type="button" data-bs-toggle="modal" data-bs-target="#modalEdit"><span class="material-symbols-outlined text-success">
-          edit
-          </span></button></td>
-      </tr>`
-      ).join('');
-  
-      todos_Eventos();
-    };
-  
-    function todos_Eventos() {
-      // ADICIONA NOVA TAREFA
-      document.querySelector("#add-tarefa").addEventListener("click", function() { // ADICIONA TAREFA
-        const tarefa = document.querySelector("#tarefa").value; // PEGA O VALOR DO INPUT
-        if (tarefa === "") {    // VERIFICA SE O INPUT ESTA VAZIO
-          alert("Digite uma tarefa!");    // SE ESTIVER VAZIO, RETORNA UM ALERTA
-          return; // E PARA A EXECUÇÃO DO CODIGO
-        }
-  
-        axios.post(`https://api.augustin06.repl.co/add`, { Tarefa: tarefa })
-          .then(function() {
-            location.reload()
+  } catch (error) {
+      console.error('Erro ao carregar despesas:', error.message);
+  }
+}
+
+// Função para adicionar uma nova despesa
+
+
+// Função para editar uma despesa
+async function editarDespesa(id, despesaAtual, valorAtual) {
+  const novoDespesa = prompt('Digite a nova despesa:', despesaAtual);
+  const novoValor = parseFloat(prompt('Digite o novo valor:', valorAtual).replace(',', '.'));
+
+  if (!novoDespesa || isNaN(novoValor)) {
+      alert('Preencha os campos corretamente.');
+      return;
+  }
+
+  try {
+      const response = await axios.put(`http://127.0.0.1:5000/update/${id}`, { despesa: novoDespesa, valor: novoValor });
+      console.log(response.data);
+
+      // Recarrega a tabela
+      carregarDespesas();
+  } catch (error) {
+      console.error('Erro ao editar despesa:', error.message);
+  }
+}
+
+// Função para excluir uma despesa
+// Função para excluir uma despesa
+function excluirDespesa(id) {
+  // Mostra uma caixa de diálogo de confirmação
+  const confirmacao = window.confirm('Deseja realmente excluir esta despesa?');
+
+  // Se o usuário confirmar, prossegue com a exclusão
+  if (confirmacao) {
+      axios.delete('http://127.0.0.1:5000/delete', { data: { id } })
+          .then(function (response) {
+              console.log(response.data);
+
+              // Recarrega a tabela
+              carregarDespesas();
           })
-          .catch(function(error) {
-            console.error(error);
+          .catch(function (error) {
+              console.error('Erro ao excluir despesa:', error.message);
           });
-      });
-  
-      // EXCLUIR TAREFA
-      document.querySelectorAll(".delete-btn").forEach(btn => {
-        btn.addEventListener("click", function(e) {
-          const id = e.target.closest("tr").querySelector("th").textContent;
-          axios.delete(`https://api.augustin06.repl.co/delete`, { data: { id: parseInt(id) } })
-            .then(function() {
-              loadTasks();
-            })
-            .catch(function(error) {
-              console.error(error);
-            });
-        });
-      });
-  
-      function updateTarefa(id, novaTarefa) {
-        axios.put(`https://api.augustin06.repl.co/update/${id}`, { TAREFA: novaTarefa })
-          .then(function() {
-            Carregar(); // RECARREGA A LISTA DEPOIS DE USAR
-          })
-          .catch(function(error) {
-            console.error(error);
-          });
+  }
+}
+
+// Função para adicionar uma nova despesa
+async function adicionarDespesa() {
+  const despesaInput = document.querySelector("#recipient-name");
+  const valorInput = document.querySelector("#recipient-valor");
+
+  const despesa = despesaInput.value;
+  const valor = parseFloat(valorInput.value.replace(',', '.'));
+
+  if (!despesa || isNaN(valor)) {
+    alert('Preencha os campos corretamente.');
+    return;
+  }
+
+  try {
+    const response = await axios.post(`http://127.0.0.1:5000/add`, { despesa: despesa, valor: valor });
+    console.log(response.data);
+
+    // Recarrega a tabela
+    carregarDespesas();
+
+    // Limpa os campos de entrada após adicionar a despesa
+    despesaInput.value = '';
+    valorInput.value = '';
+
+    // Fecha o modal de adicionar despesa
+    const modal = new bootstrap.Modal(document.getElementById('Modal3'));
+    modal.hide();
+  } catch (error) {
+    console.error('Erro ao adicionar despesa:', error.message);
+  }
+}
+
+async function carregarSalario() {
+  try {
+      const response = await axios.get('http://127.0.0.1:5000/list_salary');
+      const salarios = response.data;
+
+      if (salarios.length > 0) {
+          const ultimoSalario = salarios[salarios.length - 1].SALARIO;
+          document.getElementById('user-salary').textContent = ultimoSalario.toFixed(2);
+      } else {
+          // Caso o usuário não tenha salário cadastrado
+          document.getElementById('user-salary').textContent = 'N/A';
       }
-  
-      // EDITAR TAREFA
-      document.querySelectorAll(".edit-btn").forEach(btn => {
-        btn.addEventListener("click", function(e) {
-          const id = e.target.closest("tr").querySelector("th").textContent;
-          const novaTarefa = prompt("Digite a nova descrição da tarefa:");
-  
-          if (novaTarefa !== null) {
-            updateTarefa(parseInt(id), novaTarefa);
-          }
-        });
-      });
-  
-  
-  
-    }
-  
-    // CARREGAR TAREFAS NA PAGINA
-    function loadTasks() {
-      axios.get(`https://api.augustin06.repl.co/list`)
-        .then(function(resposta) {
-          getData(resposta.data);
-        })
-        .catch(function(error) {
-          console.error(error);
-        });
-    }
-  });
+  } catch (error) {
+      console.error('Erro ao carregar salário:', error.message);
+  }
+}
+async function editarSalario() {
+  const novoSalario = parseFloat(document.getElementById('edit-salary-input').value.replace(',', '.'));
+
+  if (isNaN(novoSalario)) {
+      alert('Por favor, insira um valor válido para o salário.');
+      return;
+  }
+
+  try {
+      const response = await axios.put('http://127.0.0.1:5000/update_salary', { salario: novoSalario });
+      console.log(response.data);
+
+      // Recarrega o salário após a edição
+      carregarSalario();
+
+      // Fecha o modal de edição
+      const modalEditSalary = new bootstrap.Modal(document.getElementById('modalEditSalary'));
+      modalEditSalary.hide();
+  } catch (error) {
+      console.error('Erro ao editar salário:', error.message);
+  }
+}
+async function carregarSalario() {
+  try {
+      const response = await axios.get('http://127.0.0.1:5000/list_salary');
+      const salarios = response.data;
+
+      if (salarios.length > 0) {
+          const ultimoSalario = salarios[salarios.length - 1].SALARIO;
+          document.getElementById('user-salary').textContent = ultimoSalario.toFixed(2);
+      } else {
+          // Caso o usuário não tenha salário cadastrado
+          document.getElementById('user-salary').textContent = 'N/A';
+      }
+  } catch (error) {
+      console.error('Erro ao carregar salário:', error.message);
+  }
+}
+
+
+// Chame a função para carregar o salário ao carregar a página
+carregarSalario();
+
+
+// Carrega as despesas ao carregar a página
+carregarDespesas();
+// Adiciona um ouvinte de evento ao botão de adicionar no modal
+document.querySelector("#addDespesaBtn").addEventListener("click", adicionarDespesa);
+
